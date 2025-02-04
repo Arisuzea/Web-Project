6b10bd571ff839bea18ef203f0e5dbd86f9928a4
Arisuzea <grldcondezz18@gmail.com>
2025-02-04 22:09:58 +0800

    Page Fixes

--- Home Page/home.js
+++ Home Page/home.js
@@ -1,15 +1,16 @@
 document.getElementById("startWatchingButton").addEventListener("click", () => {
     const videoBackground = document.getElementById("videoBackground");
     const content = document.querySelector(".content");
-
     videoBackground.style.opacity = "1";
 
     const video = document.getElementById("backgroundVideo");
+    
+
     video.play();
     video.muted = false;  
 
-   
+
     setTimeout(() => {
-      
+
     }, 1000); 
 });
